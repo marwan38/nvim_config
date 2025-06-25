a7505b94de4f1373f3300352e123b7a3f1a863ae
Marwan Mustafa Fikrat <marwan.mm@live.com>
2025-06-25 15:20:05 +0400

    updates, incl wsl clipboard

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,21 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.autoformat = false
-
 vim.g.snacks_animate = false
+
+if vim.fn.has("wsl") == 1 then
+  -- from :h clipboard-wsl
+  vim.g.clipboard = {
+    name = "win32yank-wsl",
+    copy = {
+      ["+"] = "win32yank -i --crlf",
+      ["*"] = "win32yank -i --crlf",
+    },
+    paste = {
+      ["+"] = "win32yank -o --lf",
+      ["*"] = "win32yank -o --lf",
+    },
+    cache_enabled = 0,
+  }
+end
+
